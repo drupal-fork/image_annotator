1f8ee810f061fb2d57b9dd489ebb70edcb77c488
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2012-07-19 16:09:18 +0200

    Startup for Image Annotator Testswarm tests

---
+++ image_annotator_testswarm/tests/image_annotator_rectangle_draggable.test.js
@@ -0,0 +1,35 @@
+(function ($, Drupal, window, document, undefined) {
+  Drupal.tests.iaRectangleDraggable = {
+    getInfo: function() {
+      return {
+        name: Drupal.t('Image Annotator Rectangle Draggable'),
+        description: Drupal.t('Test Image Annotator\'s draggable & resizable pointer widget'),
+        group: Drupal.t('Image Annotator'),
+        useSimulate: true
+      }
+    },
+    tests: {
+      addPointer: function ($, Drupal, window, document, undefined) {
+        return function () {
+
+        }
+      },
+      movePointer: function ($, Drupal, window, document, undefined) {
+        return function () {
+
+        }
+      },
+      resizePointer: function ($, Drupal, window, document, undefined) {
+        return function () {
+
+        }
+      },
+      removePointer: function ($, Drupal, window, document, undefined) {
+        return function () {
+
+        }
+      }
+    }
+  }
+})(jQuery, Drupal, this, this.document);
+
